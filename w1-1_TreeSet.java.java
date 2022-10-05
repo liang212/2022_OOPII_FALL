4c318b98b76ec9b051b316a0746d0395451babba
liang212 <92613488+liang212@users.noreply.github.com>
2022-10-05 17:26:24 +0800

    Add files via upload
    Weekly Hw 0922

---
+++ w1-1_TreeSet.java.java
@@ -0,0 +1,32 @@
+import java.util.TreeSet;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class w1_09291{
+  public static void main(String args[]) {
+    // 先亂數取十個數字
+    Random rand = new Random();
+    ArrayList st = new ArrayList();
+    for (int i = 0; i < 11; i++) {
+      st.add(rand.nextInt(100) + 1);
+    }
+    int count = 0;
+    for (int i = 1; i <=10; i++) {
+      System.out.println("第" + i + "個號碼：" + st.get(i));
+      count++;
+    }
+    System.out.println("物件內元素個數為:" + count);
+    System.out.println("物件內元素內容:" + st);
+    // 建立TreeSet
+    TreeSet<Integer> Tset = new TreeSet();
+    Tset.addAll(st);
+    // 結果輸出
+    System.out.println("第一個元素內容為:" + Tset.first());
+    System.out.println("最後一個元素內容為:" + Tset.last());
+    Tset=(TreeSet)Tset.subSet(30,70);
+    System.out.println("內容介於30~70者:"+ Tset);
+
+
+  }
+
+}
